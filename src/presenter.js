b5d99ca0293dc9fd117a1307c70f2a04e7e51efd
ERICK1766 <guzmanquispeerickivan@gmail.com>
2025-09-04 06:01:06 -0400

    feat: implementacion final logica de interacción  de TennisScorer en presenter.js

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,5 @@
 import sumar from "./sumador";
+import TennisScorer from "./tennisScorer.js";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
@@ -12,4 +13,29 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+
+const scorer = new TennisScorer();
+const scoreDiv = document.querySelector("#score");
+const player1Btn = document.querySelector("#player1-btn");
+const player2Btn = document.querySelector("#player2-btn");
+
+function updateScore() {
+  scoreDiv.textContent = scorer.showScore();
+
+  if (scorer.showScore().startsWith("Game for Player")) {
+    player1Btn.disabled = true;
+    player2Btn.disabled = true;
+  }
+}
+
+player1Btn.addEventListener("click", () => {
+  scorer.player1Scores();
+  updateScore();
+});
+
+player2Btn.addEventListener("click", () => {
+  scorer.player2Scores();
+  updateScore();
 });
